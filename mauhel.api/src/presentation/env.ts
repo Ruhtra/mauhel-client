ac0483cab10df7929ae4585ef108db518a67becf
Ruhtra <kawanarthurskate@gmail.com>
2024-10-29 04:11:37 -0300

    Configure prisma

--- mauhel.api/src/presentation/env.ts
+++ mauhel.api/src/presentation/env.ts
@@ -3,6 +3,8 @@ import z from "zod";
 const envScheme = z.object({
   PORT: z.string().optional().default("8000"),
   MODE: z.enum(["development", "production"]),
+  DATABASE_URL: z.string(),
+  DIRECT_URL: z.string(),
 });
 
 export const env = envScheme.parse(process.env);
