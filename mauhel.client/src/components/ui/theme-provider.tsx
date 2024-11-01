980893b63a2a14bb23a5862cc742ff8b8dfddf44
Ruhtra <kawanarthurskate@gmail.com>
2024-11-01 20:30:20 -0300

    Conigure the PWA and thema dark in app

--- mauhel.client/src/components/ui/theme-provider.tsx
+++ mauhel.client/src/components/ui/theme-provider.tsx
@@ -1,24 +1,24 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react';
 
-type Theme = 'dark' | 'light' | 'system'
+type Theme = 'dark' | 'light' | 'system';
 
 type ThemeProviderProps = {
-  children: React.ReactNode
-  defaultTheme?: Theme
-  storageKey?: string
-}
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
 
 type ThemeProviderState = {
-  theme: Theme
-  setTheme: (theme: Theme) => void
-}
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
 
 const initialState: ThemeProviderState = {
   theme: 'system',
-  setTheme: () => null
-}
+  setTheme: () => null,
+};
 
-const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
+const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
   children,
@@ -28,46 +28,61 @@ export function ThemeProvider({
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
     () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
-  )
+  );
 
   useEffect(() => {
-    const root = window.document.documentElement
+    const root = window.document.documentElement;
 
-    root.classList.remove('light', 'dark')
+    root.classList.remove('light', 'dark');
+
+    let currentTheme = theme;
 
     if (theme === 'system') {
-      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
-        .matches
+      currentTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
         ? 'dark'
-        : 'light'
-
-      root.classList.add(systemTheme)
-      return
+        : 'light';
     }
 
-    root.classList.add(theme)
-  }, [theme])
+    root.classList.add(currentTheme);
+    
+    // Atualiza a cor da barra superior
+    const themeColor = currentTheme === 'dark' ? '#000000' : '#ffffff'; // Defina suas cores
+    setThemeColor(themeColor);
+
+  }, [theme]);
+
+  const setThemeColor = (color: string) => {
+    const metaThemeColor = document.querySelector("meta[name='theme-color']");
+    if (metaThemeColor) {
+      metaThemeColor.setAttribute("content", color);
+    } else {
+      const newMetaThemeColor = document.createElement('meta');
+      newMetaThemeColor.name = 'theme-color';
+      newMetaThemeColor.content = color;
+      document.head.appendChild(newMetaThemeColor);
+    }
+  };
 
   const value = {
     theme,
     setTheme: (theme: Theme) => {
-      localStorage.setItem(storageKey, theme)
-      setTheme(theme)
-    }
-  }
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
+  };
 
   return (
     <ThemeProviderContext.Provider {...props} value={value}>
       {children}
     </ThemeProviderContext.Provider>
-  )
+  );
 }
 
 export const useTheme = () => {
-  const context = useContext(ThemeProviderContext)
+  const context = useContext(ThemeProviderContext);
 
   if (context === undefined)
-    throw new Error('useTheme must be used within a ThemeProvider')
+    throw new Error('useTheme must be used within a ThemeProvider');
 
-  return context
-}
+  return context;
+};
--- mauhel.client/vite.config.ts
+++ mauhel.client/vite.config.ts
@@ -1,13 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      globPatterns: ['**/*']
+    },
+    devOptions: {
+      enabled: true
+    },
+    includeAssets: ['**/*'],
+    manifest: {
+      name: 'Mauhel App',
+      short_name: 'MauhelApp',
+      description: 'The Mauhel App is designed to assist students at the Mauhel preparatory course in solving problems and enhancing their learning experience. With a user-friendly interface and a range of resources, this app aims to support students in their studies and improve their academic performance.',
+      theme_color: '#ffffff',
+      background_color: '#033952',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+      orientation: 'portrait',
+      icons: [
+        {
+          src: '/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  }),
+  ,],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
   }
-})
+});
